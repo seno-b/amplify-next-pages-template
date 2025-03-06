0bcd13b5b7ab170d0c644012ef93cfca85bc6e1e
seno-b <bangshinho@gmail.com>
2025-03-06 11:07:39 +0900

    added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ function deleteTodo(id: string) {
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
     
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -34,6 +34,7 @@ export default function App() {
   return (
     <main>
       <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}&apos;s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
