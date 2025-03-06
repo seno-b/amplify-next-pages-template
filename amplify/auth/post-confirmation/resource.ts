a4868e94f9263eb57c0e30056554006218935a72
seno-b <bangshinho@gmail.com>
2025-03-06 18:43:02 +0900

    feat: cognito identity provider 추가

---
+++ amplify/auth/post-confirmation/resource.ts
@@ -0,0 +1,10 @@
+import { defineFunction } from '@aws-amplify/backend';
+
+export const postConfirmation = defineFunction({
+  name: 'post-confirmation',
+  // optionally define an environment variable for your group name
+  environment: {
+    GROUP_NAME: 'EVERYONE'
+  },
+  resourceGroupName: 'auth'
+});
